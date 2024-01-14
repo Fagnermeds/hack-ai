23ebbc98bf3a8e2c550e347f828f22eaeb007283
Lucas Wanderley <lucaswanderley4@gmail.com>
2024-01-14 00:53:01 -0300

    chore: configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        32: "32px",
+      },
+      colors: {
+        // generate colors
+        "yellow-100": "#FAA831",
+        "purple-100": "#934CB0",
+        "purple-200": "#8E44AD",
+        "gray-100": "#BDC3C7",
+        "gray-200": "#B3B3B3",
+        "gray-300": "#585861",
+        "gray-400": "#575759",
+      },
+    },
   },
   plugins: [],
 };
